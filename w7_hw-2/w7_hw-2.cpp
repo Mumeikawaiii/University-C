e0593c7d2cfe747f7e0657e3f1874ee10f38b629
Mumeikawaiii <s911405@stu.tcssh.tc.edu.tw>
2023-10-28 20:28:48 +0800

    Add files via upload

---
+++ w7_hw-2/w7_hw-2.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include<math.h>
+using namespace  std;
+
+long Factorial(int num)
+{
+    /**********************
+    Factorial(int)-> long
+
+
+    ex:
+    Factorial(5)-> 120
+    Factorial(3)-> 6
+    **********************/
+    int temp = 1;
+    for (int counter = 1; counter <= num; counter++)
+    {
+        temp *= counter;
+    }
+    return temp;
+}
+
+int main()
+{
+    /*********************
+    int i;
+    cin >> i;
+    cout << Factorial(i);  //test
+    *********************/
+    int n, m, temp;
+    cout << "plz input n&m for C(n,m):";
+    cin >> n >> m;
+
+    if (n < m)             //ensure n>m
+    {
+        temp = n;
+        n = m;
+        m = temp;
+    }
+
+    temp = Factorial(n) / (Factorial(m) * Factorial(n - m));  // mian function
+    cout << temp;
+    return 0;
+
+}
