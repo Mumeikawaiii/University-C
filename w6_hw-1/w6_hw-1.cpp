ee0bc0f60f054e72e0f20592a1c764adeade43a8
Mumeikawaiii <s911405@stu.tcssh.tc.edu.tw>
2023-10-28 20:40:14 +0800

    Add files via upload

---
+++ w6_hw-1/w6_hw-1.cpp
@@ -0,0 +1,18 @@
+﻿#include <iostream>
+using namespace  std;
+
+int main()
+{
+    long long num;
+    cout << "plz input a positive integer:";
+    cin >> num;                                 //put cin into num
+    cout << "factors:";
+
+    for (int i = 2; i <= num; i++)              //find factors
+    {
+        if (num % i == 0)cout << i << " ";
+
+    }
+
+    return 0;
+}
