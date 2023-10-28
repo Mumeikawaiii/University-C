9029b9f19638fb754f6227b104544f75fa45bd3f
Mumeikawaiii <s911405@stu.tcssh.tc.edu.tw>
2023-10-28 20:40:50 +0800

    Add files via upload

---
+++ w6_hw-3/w6_hw-3.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+using namespace std;
+
+
+int main()
+{
+	char abc[30];
+	cout << "plz input a alphabet:";
+	cin >> abc;
+
+
+	for (int i = 0; abc[i]; i++)
+	{
+		if (abc[i] >= 'a' && abc[i] <= 'z') abc[i] -= 32;
+		else if (abc[i] >= 'A' && abc[i] <= 'Z') abc[i] += 32;
+	}
+	cout << "after converting:";
+	cout << abc;
+	return 0;
+}
