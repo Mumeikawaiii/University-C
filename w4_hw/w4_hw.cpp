8a743452387aa19dc468f1c7f6ce1fcf7c5bb1a8
Mumeikawaiii <s911405@stu.tcssh.tc.edu.tw>
2023-10-28 20:48:19 +0800

    Add files via upload

---
+++ w4_hw/w4_hw.cpp
@@ -0,0 +1,33 @@
+﻿#include<iostream>
+using namespace std;
+
+int main()
+{
+
+    /*****************************************
+     int F[100]={0};
+     F[0]=0;
+     F[1]=1;
+     for(int i=0;i<=20;i++)
+     {
+       F[i+2]=F[i+1]+F[i];
+       cout<<'F'<<i<<'='<<F[i]<<endl;
+     }
+   *******************************************/
+
+    long Fn, n = 2, Fn_1 = 1, Fn_2 = 0;
+    while (n < 51)
+    {
+        Fn = Fn_1 + Fn_2;
+        cout << "F" << n << "=" << Fn << endl;
+        Fn_2 = Fn_1;
+        Fn_1 = Fn;
+        n++;
+    }
+
+
+
+
+
+    return 0;
+}
