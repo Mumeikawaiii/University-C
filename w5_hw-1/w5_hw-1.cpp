117ba8552851a7b86ebe783ed855868aebaa4f1b
Mumeikawaiii <s911405@stu.tcssh.tc.edu.tw>
2023-10-28 20:43:47 +0800

    Add files via upload

---
+++ w5_hw-1/w5_hw-1.cpp
@@ -0,0 +1,23 @@
+﻿#include <iostream>
+using namespace  std;
+
+int main()
+{
+    int layer;
+    cout << "plz input the hight of ur pyramid:";
+    cin >> layer;
+
+    for (int i = 0; i < layer; i++)
+    {
+        for (int j = 0; j <= i; j++)
+        {
+            cout << "x";
+        }
+        cout << endl;
+
+    }
+
+
+
+    return 0;
+}
