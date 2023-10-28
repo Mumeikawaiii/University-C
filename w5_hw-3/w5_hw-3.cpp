894719ae8ee328f092f16a6a92833d73f78039ee
Mumeikawaiii <s911405@stu.tcssh.tc.edu.tw>
2023-10-28 20:44:24 +0800

    Add files via upload

---
+++ w5_hw-3/w5_hw-3.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+using namespace  std;
+
+int main()
+{
+    int layer, k;
+    cout << "plz input the hight of ur pyramid:";
+
+
+
+    while (cin >> layer)
+    {
+
+        for (int i = 1; i <= layer; i++)            //coculate the pyramid layer
+        {
+
+            for (int j = 0; j < i - 1; j++)
+            {
+                cout << " ";                       //print blank
+            }
+            for (int j = layer * 2 - i * 2 + 1; j > 0; j--)
+            {
+                cout << "x";
+            }
+
+            cout << endl;                          //when time to change line,endl
+            if (i == layer)k = -1;
+        }
+        if (k == -1) break;
+
+    }
+
+
+    return 0;
+}
