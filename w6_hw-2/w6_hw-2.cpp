16f350fca35d1746e3aa1b92f6be170676482f2c
Mumeikawaiii <s911405@stu.tcssh.tc.edu.tw>
2023-10-28 20:40:31 +0800

    Add files via upload

---
+++ w6_hw-2/w6_hw-2.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<math.h>                                //for sqrt
+using namespace  std;
+
+int main()
+{
+    long long num;
+    cout << "plz input a positive integer:";
+
+    while (cin >> num)
+    {
+        long long r;                            //remainder
+        cout << num << "=";
+        for (int i = 2; i <= sqrt(num); i++)    //find prime factor
+        {
+
+            r = num % i;
+            while (r == 0)
+            {
+                num /= i;
+                cout << i;
+                r = num % i;
+                if (num == 1)break;             //void print too more "*"
+                cout << "*";
+            }
+        }
+        if (num != 1)cout << num;               //number "1" is not prime factor
+
+    }
+
+
+
+    return 0;
+}
